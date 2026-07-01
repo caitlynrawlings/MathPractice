56b6c22966884d1de6f21243c48e9cac8afbbb26
crawlin <crawlin@uw.edu>
2026-06-30 22:49:49 -0700

    fix workflow

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,24 @@
 //   reactStrictMode: true,
 // };
 
+// const nextConfig = {
+//   output: 'export', // Outputs a static 'out' folder instead of starting a Node server
+//   images: {
+//     unoptimized: true, // Required because Next.js default Image Optimization requires a production server
+//   },
+//   // Only include basePath and assetPrefix if your repository is NOT a User/Organization page (i.e. username.github.io)
+//   basePath: process.env.NODE_ENV === 'production' ? '/MathPractice' : '',
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/MathPractice/' : '',
+// };
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Outputs a static 'out' folder instead of starting a Node server
+  output: "export",
   images: {
-    unoptimized: true, // Required because Next.js default Image Optimization requires a production server
+    unoptimized: true,
   },
-  // Only include basePath and assetPrefix if your repository is NOT a User/Organization page (i.e. username.github.io)
-  basePath: process.env.NODE_ENV === 'production' ? '/MathPractice' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/MathPractice/' : '',
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
